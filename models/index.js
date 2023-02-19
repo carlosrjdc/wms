a79fd318d030076adec433438e5bcbe44035054d
Carlos Roberto <uppaplicativo@gmail.com>
2023-02-18 22:45:20 -0300

    terceirdo

--- models/index.js
+++ models/index.js
@@ -24,7 +24,8 @@ fs
       file !== basename &&
       file.slice(-3) === '.js' &&
       file.indexOf('.test.js') === -1
-    );
+    )
+    
   })
   .forEach(file => {
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
